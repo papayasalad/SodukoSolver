264a4de7cc2af968610af5345c66345a38f57a54
papayasalad <papayasaladgo@gmail.com>
2019-04-28 22:50:15 -0700

    first commit to branch, with tests

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,5 +1,8 @@
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Board {
     private List<List<Integer>> board;
@@ -25,5 +28,98 @@ public class Board {
         }
         res.deleteCharAt(res.length() - 1);
         return res.toString();
+    }
+
+    public boolean isValid() {
+        return this.cellsValid() && this.rowsValid() && this.colsValid() && this.gridsValid();
+    }
+
+    public boolean isSolved() {
+        return this.isFilled() && this.isValid();
+    }
+
+    public boolean cellsValid() {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.get(i).size(); j++) {
+                if (!(board.get(i).get(j) >= 0 && board.get(i).get(j) <= 9 || board.get(i).get(j) == null)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean rowsValid() {
+        for (int i = 0; i < board.size(); i++) {
+            Set<Integer> rowsBank = new HashSet<>();
+            for (int j = 0; j < board.get(i).size(); j++) {
+                if(board.get(i).get(j) == null) {
+                    continue;
+                } else {
+                    if (!rowsBank.contains(board.get(i).get(j))) {
+                        rowsBank.add(board.get(i).get(j));
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean colsValid() {
+        for (int j = 0; j < board.get(0).size(); j++) {
+            Set<Integer> colsBank = new HashSet<>();
+            for (int i = 0; i < board.size(); i++) {
+                if(board.get(i).get(j) == null) {
+                    continue;
+                } else {
+                    if (!colsBank.contains(board.get(i).get(j))) {
+                        colsBank.add(board.get(i).get(j));
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean gridsValid() {
+        for (int i = 0; i <= 6; i += 3) {
+            for (int j = 0; j <= 6; j += 3) {
+                if(!smallGridValid(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean smallGridValid(int rowStart, int colStart) {
+        Set<Integer> gridBank = new HashSet<>();
+        for (int i = rowStart; i < rowStart + 3; i++) {
+            for (int j = colStart; j < colStart + 3; j++) {
+                if(board.get(i).get(j) == null) {
+                    continue;
+                } else {
+                    if (!gridBank.contains(board.get(i).get(j))) {
+                        gridBank.add(board.get(i).get(j));
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isFilled() {
+        for (List<Integer> row : board) {
+            if (row.contains(null)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
